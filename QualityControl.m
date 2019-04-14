653421afadd230dec879140f8d00e54f7aa213f3
StasMakarchuk <49611600+StasMakarchuk@users.noreply.github.com>
2019-04-14 13:41:54 +0100

    Update QualityControl.m
    Now code removes near intensity peaks only within one frame

--- QualityControl.m
+++ QualityControl.m
@@ -12,7 +12,7 @@ Author: Ezra Bruggeman, Laser Analytics Group
 Last updated: 21 Aug 2018
 %}
 
-function correctedLocFile = QualityControl(localizations, r_min, sigma_max)
+function [correctedLocFile distAll] = QualityControl(localizations, r_min, sigma_max)
 
 %% Control 1: Filter out beads that are too big
 localizations = localizations(localizations.sigma < sigma_max,:);
@@ -30,17 +30,56 @@ figure;
 scatter(coordinates(:,1), coordinates(:,2));
 hold on;
 
+
+
+
+
 % Calculate the distance between every set of points
-distAll = pdist2(coordinates,coordinates);
+% Stas 09.04.19
 
-% Find the points that are closer together than r_min
-tooClose = distAll < r_min;
-num_TooClose = (sum(tooClose) - 1);
+% check frame by frame
+n=2; Coord_Frame = localizations(n,:); Current_Frame = localizations(1,2);
+correctedLocFile=[];
+while n<size(localizations,1)
+   %check # of frames, if the same, keep add positions to Coord_Frame
+   if localizations(n,2)==Current_Frame 
+       Coord_Frame = [Coord_Frame; localizations(n,:)]; 
+       n=n+1;
+   else
+       Current_Frame = localizations(n,2);
+       
+      %if next frame detected filter out within one frame
+      distAll = pdist2(Coord_Frame,Coord_Frame);
+      
+      % Find the points that are closer together than r_min
+      tooClose = distAll < r_min;
+      num_TooClose = (sum(tooClose) - 1);
 
-% Get filtered matrix 
-correctedLocFile = localizations;
-noNearNeighbour = num_TooClose ~= 0;
-correctedLocFile(noNearNeighbour, :) = [];
+      % Get filtered matrix 
+      correctedLocFileOneFrame = Coord_Frame;
+      noNearNeighbour = num_TooClose ~= 0;
+      correctedLocFileOneFrame(noNearNeighbour, :) = [];
+      if isempty(correctedLocFileOneFrame)==0
+      correctedLocFile = [correctedLocFile; correctedLocFileOneFrame];
+      end
+      Coord_Frame=[];
+   end
+      
+end
+
+% distAll = pdist2(coordinates,coordinates);
+% 
+% 
+% 
+% % Find the points that are closer together than r_min
+% tooClose = distAll < r_min;
+% num_TooClose = (sum(tooClose) - 1);
+% 
+% % Get filtered matrix 
+% correctedLocFile = localizations;
+% noNearNeighbour = num_TooClose ~= 0;
+% correctedLocFile(noNearNeighbour, :) = [];
+
 
 % Get matrix containing only rows that were filtered out (for visualization purposes)
 deletedLocalizations = localizations;
